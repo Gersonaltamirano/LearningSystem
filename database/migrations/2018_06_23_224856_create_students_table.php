bc4ba3b90428b5d75fcc32850b87715394796818
Gerson Altamirano <gersonaltamirano@gmail.com>
2018-06-24 10:48:08 -0600

    Fix tite por title

--- database/migrations/2018_06_23_224856_create_students_table.php
+++ database/migrations/2018_06_23_224856_create_students_table.php
@@ -17,7 +17,7 @@ class CreateStudentsTable extends Migration
             $table->increments('id');
             $table->unsignedInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->string('tite')->nullable();
+            $table->string('title')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
