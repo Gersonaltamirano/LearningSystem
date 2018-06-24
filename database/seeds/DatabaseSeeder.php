8ad21391c070c1e5d131bcad71af626caee9bec6
Gerson Altamirano <gersonaltamirano@gmail.com>
2018-06-24 11:32:36 -0600

    Definiendo Seed de Cursos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -48,6 +48,12 @@ class DatabaseSeeder extends Seeder
         factory(\App\Level::class, 1)->create(['name' => 'Advance']);
         factory(\App\Category::class, 5)->create();
 
+        factory(\App\Course::class, 50)->create()
+            ->each(function (\App\Course $c) {
+               $c->goals()->saveMany(\App\Goal::class,2)->create();
+               $c->requirements()->saveMany(\App\Requirement::class,4)->create();
+            });
+
 
     }
 }
